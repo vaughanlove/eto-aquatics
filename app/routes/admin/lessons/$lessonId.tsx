9f52106e158206fb05e30ff794ff43a8c32940ae
vaughanlove <vaughanlove1@gmail.com>
2022-08-08 16:52:08 -0400

    added timeslot add/remove

--- app/routes/admin/lessons/$lessonId.tsx
+++ app/routes/admin/lessons/$lessonId.tsx
@@ -28,27 +28,43 @@ export const action: ActionFunction = async ({
 }) => {
 
   const form = await request.formData();
+  const input_type = form.get("input_type");
   const availableSpots = parseInt(form.get("availableSpots"));
   const instructor = form.get("instructor");
+  const start = new Date(form.get("start"));
+  const end = new Date(form.get("end"));
   const classMinutes = parseInt(form.get("classMinutes"));
   const totalHours = parseInt(form.get("totalHours"));
-  const lessonCost = parseInt(form.get("lessonCost"))
-  const lessonId = form.get("lessonId")
-  
+  const lessonCost = parseInt(form.get("lessonCost"));
+  const lessonId = form.get("lessonId");
+  const timeslotId = form.get("timeslotId");
   // we do this type check to be extra sure and to make TypeScript happy
   // we'll explore validation next!
-  if (
-    typeof availableSpots !== "number" ||
-    typeof classMinutes !== "number" ||
-    typeof totalHours !== "number" ||
-    typeof lessonId !== "string"
-  ) {
-    console.log()
-    throw new Error(`Form not submitted correctly.`);
+  // if (
+  //   typeof availableSpots !== "number" ||
+  //   typeof classMinutes !== "number" ||
+  //   typeof totalHours !== "number" ||
+  //   typeof lessonId !== "string"
+  // ) {
+  //   console.log()
+  //   throw new Error(`Form not submitted correctly.`);
+  // }
+
+  switch(input_type){
+    case "createLesson": {
+      await db.lesson.update({data: {availableSpots: availableSpots, instructor: instructor, classMinutes: classMinutes,totalHours: totalHours, lessonCost: lessonCost}, where: {id: lessonId}, select: {id: true}})
+      return redirect(`/admin/lessons/${lesson.id}`);
+    }
+    case "removeLesson": {
+      await db.timeslot.delete({where: {id: timeslotId}})
+      return null
+    }
+    case "addLessonTime": {
+      await db.timeslot.create({data: {start: start.toISOString(), end: end.toISOString(), for: {connect: {id: lessonId}}}})
+      return null
+    }
   }
 
-  const lesson = await db.lesson.update({data: {availableSpots: availableSpots, instructor: instructor, classMinutes: classMinutes,totalHours: totalHours, lessonCost: lessonCost}, where: {id: lessonId}, select: {id: true}})
-  return redirect(`/admin/lessons/${lesson.id}`);
 };
 
 export default function LessonRoute() {
@@ -56,44 +72,63 @@ export default function LessonRoute() {
     return (
       <div>
         <form method="post">
-        <div>
-          <label>
-           <input type="hidden" name="lessonId" defaultValue={data.lesson.id} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Available Spots: <input type="text" name="availableSpots" defaultValue={data.lesson.availableSpots} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Instructor: <input type="text" name="instructor" defaultValue={data.lesson.instructor} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Class Minutes: <input type="text" name="classMinutes" defaultValue={data.lesson.classMinutes} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Total Hours: <input type="text" name="totalHours" defaultValue={data.lesson.totalHours} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Lesson Cost: <input type="text" name="lessonCost" defaultValue={data.lesson.lessonCost} />
-          </label>
-        </div>
-        <div>
-          <button type="submit" className="bg-green-200">
-            Update
+          <input type="hidden" name="input_type" value="createLesson"/>
+
+          <div>
+            <label>
+            <input type="hidden" name="lessonId" defaultValue={data.lesson.id} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Available Spots: <input type="text" name="availableSpots" defaultValue={data.lesson.availableSpots} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Instructor: <input type="text" name="instructor" defaultValue={data.lesson.instructor} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Class Minutes: <input type="text" name="classMinutes" defaultValue={data.lesson.classMinutes} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Total Hours: <input type="text" name="totalHours" defaultValue={data.lesson.totalHours} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Lesson Cost: <input type="text" name="lessonCost" defaultValue={data.lesson.lessonCost} />
+            </label>
+          </div>
+          <div>
+            <button type="submit" className="bg-green-200">
+              Update
+            </button>
+          </div>
+        </form>
+        {data.lesson.times.map((timeslot) => 
+        (<div>{timeslot.start} - {timeslot.end} 
+          <form method="post">
+            <input type="hidden" name="input_type" value="removeLesson"/>
+            <input type="hidden" name="timeslotId" value={timeslot.id}/>
+            <button type="submit">
+              Remove Time
+            </button>
+          </form>
+        </div>))}
+        <form method="post">
+          <input type="hidden" name="lessonId" defaultValue={data.lesson.id} />
+          <input type="hidden" name="input_type" value="addLessonTime"/>
+          <input type="datetime-local" name="start" />
+          <input type="datetime-local" name="end"/>
+          <button type="submit">
+            Add Time
           </button>
-        </div>
-      </form>
-        <Link to={"/"}> Home </Link>
-        {data.lesson.times.map((timeslot) => (<div>{timeslot.start} - {timeslot.end}</div>))}
+        </form>
       </div>
     );
   }
